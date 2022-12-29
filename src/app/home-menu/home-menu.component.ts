1f0350933dd9be454fdc7eaf76a4a26e5a082785
E-Jacques <emil00j77@gmail.com>
2022-12-29 16:39:36 +0100

    Clicking hot music will know allow to play the music

--- src/app/home-menu/home-menu.component.ts
+++ src/app/home-menu/home-menu.component.ts
@@ -3,33 +3,41 @@ import { Router } from '@angular/router';
 import { MusicDto } from 'src/types/api-dto/MusicDto';
 import { PlaylistsDto } from 'src/types/api-dto/PlaylistsDto';
 import { MockApiHandlerService } from '../api-services/mock-api-handler.service';
+import { EventBusService } from '../event-bus.service';
+import { EventData, EventDataEnum } from '../event-data';
 
 @Component({
   selector: 'app-home-menu',
   templateUrl: './home-menu.component.html',
-  styleUrls: ['./home-menu.component.scss']
+  styleUrls: ['./home-menu.component.scss'],
 })
 export class HomeMenuComponent implements OnInit {
   protected playlistList: PlaylistsDto[] = [];
   protected hitMusicList: MusicDto[] = [];
 
-  constructor(private mockApiHandler: MockApiHandlerService, private router: Router) { }
+  constructor(
+    private eventBus: EventBusService,
+    private mockApiHandler: MockApiHandlerService,
+    private router: Router
+  ) {}
 
   async ngOnInit(): Promise<void> {
-    this.playlistList = await this.mockApiHandler.fetchAllPlaylist()
-    this.hitMusicList = await this.mockApiHandler.fetchHitMusic()
+    this.playlistList = await this.mockApiHandler.fetchAllPlaylist();
+    this.hitMusicList = await this.mockApiHandler.fetchHitMusic();
   }
 
   protected async getMusicAuthorsFormated(musicId: number): Promise<String> {
     const artists = await this.mockApiHandler.fetchMusicArtistsById(musicId);
-    return artists.map(a => a.name).join(", ")
+    return artists.map((a) => a.name).join(', ');
   }
 
-  redirectToMusic(musicId: number): void {
-    this.router.navigate(["music", musicId]);
+  playMusic(musicId: number): void {
+    this.eventBus.emit(
+      new EventData(EventDataEnum.ADD_MUSIC_TO_QUEUE, musicId)
+    );
   }
 
   redirectToPlaylist(playlistId: number): void {
-    this.router.navigate(["playlist", playlistId]);
+    this.router.navigate(['playlist', playlistId]);
   }
 }
