b5f6d568ae42ee1a9bba40d882d685eaa6ebc496
E-Jacques <emil00j77@gmail.com>
2023-01-02 10:02:22 +0100

    Update UsersDto to remove password from object on frontend

--- src/types/api-dto/UsersDto.ts
+++ src/types/api-dto/UsersDto.ts
@@ -1,9 +1,9 @@
 export interface UsersDto {
-    userID: number
-    username: String
-    password: String
-    email: String
-    firstName: String
-    lastName: String
-    Roles_roleID: number
+  userID: number;
+  username: String;
+  // password: String
+  email: String;
+  firstName: String;
+  lastName: String;
+  Roles_roleID: number;
 }
