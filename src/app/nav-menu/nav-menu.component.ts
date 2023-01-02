90fc39c6a09a77f3c9b4b81b98128905bc67d32f
E-Jacques <emil00j77@gmail.com>
2023-01-02 11:52:32 +0100

    Navbar update according to login state

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MockAuthService } from '../auth-services/mock-auth.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.scss']
+  styleUrls: ['./nav-menu.component.scss'],
 })
 export class NavMenuComponent {
-
+  constructor(protected authService: MockAuthService) {}
 }
