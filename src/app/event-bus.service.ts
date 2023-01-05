c26b327cad6def8e40cd9fc2e3088fe1e13f99c7
E-Jacques <emil00j77@gmail.com>
2023-01-05 19:48:10 +0100

    Change due to prettier

--- src/app/event-bus.service.ts
+++ src/app/event-bus.service.ts
@@ -3,25 +3,26 @@ import { filter, map, Subject, Subscription } from 'rxjs';
 import { EventData, EventDataEnum } from './event-data';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 /**
  * source: https://levelup.gitconnected.com/communicate-between-angular-components-using-rxjs-7221e0468b2
  */
 export class EventBusService {
-
   private subject: Subject<EventData> = new Subject();
 
-  constructor() { }
+  constructor() {}
 
   public emit(event: EventData) {
     this.subject.next(event);
   }
 
   public on(name: EventDataEnum, action: any): Subscription {
-    return this.subject.pipe(
-      filter((e) => e.getName() === name),
-      map((e: EventData) => e.getValue())
-    ).subscribe(action)
+    return this.subject
+      .pipe(
+        filter((e) => e.getName() === name),
+        map((e: EventData) => e.getValue())
+      )
+      .subscribe(action);
   }
 }
