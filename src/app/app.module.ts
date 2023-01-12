dc60c6a3ec105356862ff753cc3ccb9cc6514b53
E-Jacques <55321938+E-Jacques@users.noreply.github.com>
2023-01-12 09:58:01 +0100

    Add pipe to depedencies in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MusicPageComponent } from './music-page/music-page.component';
 import { AddFormPageComponent } from './add-form-page/add-form-page.component';
 import { MultipleSelectComponent } from './multiple-select/multiple-select.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ValidDurationPipe } from './valid-duration.pipe';
 
 @NgModule({
   declarations: [
@@ -51,6 +52,7 @@ import { HttpClientModule } from '@angular/common/http';
     MusicPageComponent,
     AddFormPageComponent,
     MultipleSelectComponent,
+    ValidDurationPipe,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   providers: [],
